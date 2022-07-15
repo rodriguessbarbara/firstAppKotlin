538439e5f8081f0737b4d51cb5ae9398949a3eed
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2022-07-15 14:52:25 -0300

    adicionado view e button

--- app/src/main/java/com/example/firstappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firstappkotlin/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.firstappkotlin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvResultado = findViewById<TextView>(R.id.textoView)
+        val nome = findViewById<EditText>(R.id.editTextTextName)
+        val btnEnviar = findViewById<Button>(R.id.btnEnviar)
+
+        btnEnviar.setOnClickListener {
+            tvResultado.text = "Olá, ${nome.text}!"
+        }
     }
 }
